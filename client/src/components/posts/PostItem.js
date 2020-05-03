cace388f24e2aa321bff0418535b696588a7195f
Kota Watanabe <kota3316@gmail.com>
2020-05-02 18:49:17 -0700

    addComment and RemoveComment actions

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
 import { addLike, removeLike, deletePost } from '../../actions/post';
-import avatar from  '../../img/doctor-avatar.png'
+import avatar from  '../../img/doctor-avatar.png';
 
 const PostItem = ({addLike, removeLike, deletePost,auth, 
     post: {
@@ -15,10 +15,12 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
         likes,
         comments,
         date
-}}) => 
+    },
+    showActions
+}) => 
     <div className="post bg-white p-1 my-1">
           <div>
-          <Link to={`/profile/${user}`}>
+            <Link to={`/profile/${user}`}>
               <img
                 className="round-img"
                 src={avatar}
@@ -34,7 +36,9 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
              <p className="post-date">
                 Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
-            <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
+
+            {showActions && <Fragment>
+              <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
               <i className="fas fa-thumbs-up">{' '}</i>
               <span>{likes.length > 0 && (
               <span>{likes.length}</span>
@@ -43,7 +47,7 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
             <button type="button" className="btn btn-light" onClick={() => removeLike(_id)}>
               <i className="fas fa-thumbs-down"></i>
             </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
+            <Link to={`/posts/${_id}`} className="btn btn-primary">
               Discussion {' '}
               {comments.length > 0 && (
               <span className='comment-count'>{comments.length}</span>
@@ -58,20 +62,25 @@ const PostItem = ({addLike, removeLike, deletePost,auth,
                     <i className="fas fa-times"></i>
                 </button>
             )}
+            </Fragment>}
           </div>
     </div>
+
+PostItem.defaultProps = {
+  showActions: true
+}
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
-}
-
-const mapStateToProps = state => ({
-    auth: state.auth,
     addLike:PropTypes.func.isRequired,
     removeLike:PropTypes.func.isRequired,
     deletePost:PropTypes.func.isRequired,
-    post: PropTypes.object.isRequired
+}
+
+const mapStateToProps = state => ({
+    auth: state.auth
 })
 
 export default connect(mapStateToProps,{ addLike, removeLike, deletePost })(PostItem);
+
